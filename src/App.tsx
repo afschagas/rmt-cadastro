a635b0fb8ec26f2f4a2b178ea34f01e98a44a661
Alexandre Chagas <afschagas@gmail.com>
2022-03-04 14:56:29 -0300

    feat: Adicionado tema escruro e troca de thema

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { AppThemeProvider } from './shared/contexts';
 import { AppRoutes } from './routes';
-import { LightTheme } from './shared/themes';
+
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter >
         <AppRoutes />
       </BrowserRouter>
-
-    </ThemeProvider>
-
-
+    </AppThemeProvider>
   );
 }
 
