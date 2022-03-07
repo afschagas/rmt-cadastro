52ac568b5d18e8e707fbf9b988f37780ee441aa2
Alexandre Chagas <afschagas@gmail.com>
2022-03-07 07:44:28 -0300

    Criando layout base para as páginas

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import { LayoutBaseDePagina } from '../../shared/layouts';
+
+
+export const Dashboard = () => {
+
+  return (
+    <LayoutBaseDePagina titulo='Página inicial'>
+
+      Testando
+
+    </LayoutBaseDePagina>
+  );
+};
