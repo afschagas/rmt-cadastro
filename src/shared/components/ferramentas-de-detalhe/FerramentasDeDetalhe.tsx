eb1744f099a9168504f20f90982daea1f68f23a3
Alexandre Chagas <afschagas@gmail.com>
2022-03-25 15:20:32 -0300

    Finalizando os services de acesso ao backend

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -94,7 +94,7 @@ export const FerramentasDeDetalhes: React.FC<IFerramentasDeDetalhesProps> = ({
           startIcon={<Icon>save</Icon>}
         >
           <Typography variant='button' whiteSpace='nowrap' textOverflow='ellipsis' overflow='hidden'>
-            Salvar e voltar
+            Salvar e fechar
           </Typography>
 
         </Button>
