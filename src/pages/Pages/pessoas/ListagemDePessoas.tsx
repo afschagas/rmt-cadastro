90112e72cd248c90c9914bf549b060b2b965af7c
Alexandre Chagas <afschagas@gmail.com>
2022-04-07 10:04:52 -0300

    Adicionando loading na listagem de pessoas

--- src/pages/Pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/Pages/pessoas/ListagemDePessoas.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 
 // Components
+import { IListagemPessoa, PessoasService, } from '../../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../../shared/components';
 import { LayoutBaseDePagina } from '../../../shared/layouts';
-import { PessoasService } from '../../../shared/services/api/pessoas/PessoasService';
+import { useDebounce } from '../../../shared/hooks/UseDebounce';
+import { Environment } from '../../../shared/environment';
 
 // Libs
 //import { toast } from 'react-toastify';
@@ -12,24 +15,36 @@ import { PessoasService } from '../../../shared/services/api/pessoas/PessoasServ
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalCount, setTotalCount] = useState(0)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
 
-    PessoasService.getAll(1, busca)
-      //Quando o getAll retornar faça alguma coisa
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
+          }
+        });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [busca]);
-
 
   return (
     <LayoutBaseDePagina
@@ -43,13 +58,44 @@ export const ListagemDePessoas: React.FC = () => {
         />
       }
     >
+      <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate' />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
 
     </LayoutBaseDePagina>
   );
 };
-
-
-
 
 
 
